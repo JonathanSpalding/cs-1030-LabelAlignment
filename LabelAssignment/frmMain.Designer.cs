6e48955a587cd221aa3ba545080de89981fbe82d
JonathanSpalding <Jon.Spalding@hotmail.com>
2016-04-20 11:12:46 -0600

    CS 1030 C#

---
+++ LabelAssignment/frmMain.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace LabelAssignment
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnCenter = new System.Windows.Forms.Button();
+            this.lblData = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLeft.Location = new System.Drawing.Point(24, 12);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(67, 51);
+            this.btnLeft.TabIndex = 0;
+            this.btnLeft.Text = "Left Justify";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRight.Location = new System.Drawing.Point(194, 12);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(67, 51);
+            this.btnRight.TabIndex = 1;
+            this.btnRight.Text = "Right Justify";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnCenter
+            // 
+            this.btnCenter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCenter.Location = new System.Drawing.Point(109, 12);
+            this.btnCenter.Name = "btnCenter";
+            this.btnCenter.Size = new System.Drawing.Size(67, 51);
+            this.btnCenter.TabIndex = 2;
+            this.btnCenter.Text = "Center";
+            this.btnCenter.UseVisualStyleBackColor = true;
+            this.btnCenter.Click += new System.EventHandler(this.btnCenter_Click);
+            // 
+            // lblData
+            // 
+            this.lblData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblData.BackColor = System.Drawing.Color.White;
+            this.lblData.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblData.Location = new System.Drawing.Point(24, 79);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(237, 24);
+            this.lblData.TabIndex = 3;
+            this.lblData.Text = "This is a label.";
+            this.lblData.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblData.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblData);
+            this.Controls.Add(this.btnCenter);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Name = "frmMain";
+            this.Text = "Label Alignment";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnCenter;
+        private System.Windows.Forms.Label lblData;
+    }
+}
+
---
+++ LabelAssignment/frmMain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabelAssignment
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            lblData.TextAlign = ContentAlignment.MiddleLeft;
+            EnableButtons((Button)sender);
+        }
+
+        private void btnCenter_Click(object sender, EventArgs e)
+        {
+            lblData.TextAlign = ContentAlignment.MiddleCenter;
+            EnableButtons((Button)sender);
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            lblData.TextAlign = ContentAlignment.MiddleRight;
+            EnableButtons((Button)sender);
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            EnableButtons(btnLeft);
+        }
+
+        private void EnableButtons(Button sender)
+        {
+            btnLeft.Enabled = true;
+            btnCenter.Enabled = true;
+            btnRight.Enabled = true;
+            sender.Enabled = false;
+        }
+    }
+}
